8eb9599007f97e80d1deb10eff5450fe90bcbb53
Brian Stefan Jensen <brstj@aarhus.dk>
2010-07-14 15:12:49 +0200

    Fixed breadcrumb (added menu_breadcrumb)

--- sites/all/themes/urbanmediaspace/templates/page/page.tpl.php
+++ sites/all/themes/urbanmediaspace/templates/page/page.tpl.php
@@ -83,14 +83,14 @@
 
     <div id="main">
 
-      <?php print $messages; ?>
-      <?php print $help; ?>
-
       <?php if ($content_top): ?>
         <div id="content-top" class="region region-content-top">
       <?php print $content_top; ?>
         </div> <!-- /#content-top -->
       <?php endif; ?>
+
+      <?php print $messages; ?>
+      <?php print $help; ?>
 
       <?php print $breadcrumb; ?>
 
