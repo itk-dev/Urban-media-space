739282af6462b7ee32323ba23912d243391eb1b0
Brian Stefan Jensen <brstj@aarhus.dk>
2010-09-06 16:21:58 +0200

    Fixed styling and markup after context fix

--- sites/all/themes/urbanmediaspace_mothership/templates/page/page.tpl.php
+++ sites/all/themes/urbanmediaspace_mothership/templates/page/page.tpl.php
@@ -39,16 +39,6 @@
           <?php if ($site_logo): ?>
             <span id="logo"><?php print $site_logo; ?></span>
           <?php endif; ?>
-          <?php if ($site_name): ?>
-            <?php if ($title): ?>
-              <div id="site-name" class="grid-3 omega"><a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a></div>
-              <?php else: /* Use h1 when the content title is empty */ ?>
-                ¤page¤<h1 id="site-name" class="grid-3 omega"><a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="nofollow"><?php print $site_name; ?></a></h1>
-              <?php endif; ?>
-            <?php endif; ?>
-          <?php if ($site_slogan): ?>
-            <div id="site-slogan" class="grid-3 omega"><?php print $site_slogan; ?></div>
-          <?php endif; ?>
         </div> <!--/#branding -->
 
         <?php if ($main_menu_links || $secondary_menu_links): ?>
@@ -66,12 +56,6 @@
       </div> <!--/#header-inner -->
 
       <div id="site-subheader" class="prefix-1 suffix-1 clear-block">
-        <?php if ($mission): ?>
-        <div id="mission" class="<?php print ns('grid-14', $header, 7); ?>">
-          <?php print $mission; ?>
-        </div>
-        <?php endif; ?>
-
         <?php if ($header): ?>
         <div id="header-region" class="region <?php print ns('grid-16', $mission, 7); ?> clear-block">
           <?php print $header; ?>
@@ -118,15 +102,17 @@
                 </div> <!--//end #sidebar-right-inner -->
                 <?php endif; ?>
 
+                <?php if ($content_bottom): ?>
+                  <div id="content-bottom" class="region region-content_bottom">
+                    <?php print $content_bottom; ?>
+                  </div> <!-- /#content-bottom -->
+                <?php endif; ?>
+
               </div> <!-- /#main-content -->
 
               <?php print $feed_icons; ?>
 
-              <?php if ($content_bottom): ?>
-                <div id="content-bottom" class="region region-content_bottom">
-                  <?php print $content_bottom; ?>
-                </div> <!-- /#content-bottom -->
-              <?php endif; ?>
+
 
             </div> <!-- // #content-inner -->
           </div> <!-- //#content -->
