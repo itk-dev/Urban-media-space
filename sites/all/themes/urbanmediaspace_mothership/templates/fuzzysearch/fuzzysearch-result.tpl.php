d8c5e6a72d1e6c56c0eceeda94865de9c57661a7
Brian Stefan Jensen <brstj@aarhus.dk>
2010-10-01 14:16:31 +0200

    Fixed styling for search results and pager

--- sites/all/themes/urbanmediaspace_mothership/templates/fuzzysearch/fuzzysearch-result.tpl.php
+++ sites/all/themes/urbanmediaspace_mothership/templates/fuzzysearch/fuzzysearch-result.tpl.php
@@ -16,23 +16,12 @@
 <?php if (!$page): ?>
   <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
 <?php endif; ?>
-
-  <div class="meta">
-  <?php if ($submitted): ?>
-    <span class="submitted"><?php print $submitted ?></span>
-  <?php endif; ?>
-
-  <?php if ($terms): ?>
-    <div class="terms terms-inline"><?php print $terms ?></div>
-  <?php endif;?>
+<div class="content">
+  <?php print $content ?>
+  <div>
+    &#187; <a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a>
   </div>
-
-  <div class="content">
-    <?php print $content ?>
-    <div>
-      &#187; <a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a>
-    </div>
-  </div>
+</div>
 
   <?php print $links; ?>
 </div>
