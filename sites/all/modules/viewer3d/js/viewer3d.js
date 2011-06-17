48348ad2a97948e038b724795ac8eb0d13420e72
Jesper Kristensen <cableman@linuxdev.dk>
2011-06-17 11:52:43 +0200

    Added the last flash configuration options

--- sites/all/modules/viewer3d/js/viewer3d.js
+++ sites/all/modules/viewer3d/js/viewer3d.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
     
-   var viewerSettings = Drupal.settings.viewer3d;
-   swfobject.embedSWF(viewerSettings['flash_location']+'/swf_viewer/BuildingViewer.swf', "building-viewer", "940", "600", "10.0.0", viewerSettings['flash_location']+"/scripts/expressInstall.swf",{width:'1045',DaluxBuildingViewServerURL:"http://prod.dalux.dk/mmhus/output/0806/&OverviewURL=http://prod.dalux.dk/mmhus/overview/&currentLocation=8&angle=4.54&angle2=6.10&showTopBar=true&showLog=true"},{allowFullScreen:"true",allowScriptAccess:"sameDomain",wmode: "1"});
-
-   alert(viewerSettings['flash_location']+'/swf_viewer/BuildingViewer.swf');
+  var viewerSettings = Drupal.settings.viewer3d;
+  swfobject.embedSWF(viewerSettings['flash_location']+'/swf_viewer/BuildingViewer.swf',
+                     "building-viewer",
+                     "940",
+                     "600",
+                     "10.0.0",
+                     viewerSettings['flash_location']+"/scripts/expressInstall.swf",
+                     {width:'1045',DaluxBuildingViewServerURL:viewerSettings['data']+'&OverviewURL='+viewerSettings['overviewURL']+'&currentLocation='+viewerSettings['currentLocation']+'&angle='+viewerSettings['angle']+'&angle2='+viewerSettings['angle2']+'&showTopBar='+viewerSettings['showTopBar']+'&showLog='+viewerSettings['showLog']},
+                     {allowFullScreen:"true", allowScriptAccess:"sameDomain", wmode: "1"});
 
   $('#building-viewer-nav-wrapper li a').tipsy({gravity: 's',fade: true});
 
@@ -18,38 +23,38 @@ $(document).ready(function() {
     app.gotoLocation(pos, angle1, angle2);
   }
 
-   function getObject(obj) {
-       var theObj;
-       if (document.all) {
-           if (typeof obj == "string") {
-               return document.all(obj);
-           } else {
-               return obj.style;
-           }
-       }
-       if (document.getElementById) {
-           if (typeof obj == "string") {
-               return document.getElementById(obj);
-           } else {
-               return obj.style;
-           }
-       }
-       return null;
-   }
+  function getObject(obj) {
+    var theObj;
+    if (document.all) {
+      if (typeof obj == "string") {
+        return document.all(obj);
+      } else {
+        return obj.style;
+      }
+    }
+    if (document.getElementById) {
+      if (typeof obj == "string") {
+        return document.getElementById(obj);
+      } else {
+        return obj.style;
+      }
+    }
+    return null;
+  }
 
-   function thisMovie(movieName) {
-       // IE and Netscape refer to the movie object differently.
-       // This function returns the appropriate syntax depending on the browser.
+  function thisMovie(movieName) {
+    // IE and Netscape refer to the movie object differently.
+    // This function returns the appropriate syntax depending on the browser.
 
-       var IE = /*@cc_on!@*/false
+    var IE = /*@cc_on!@*/false
 
 
-       if (IE) {
-        return window[movieName];
-       } else {
-        return window.document[movieName];
-       }
-   }
+    if (IE) {
+      return window[movieName];
+    } else {
+      return window.document[movieName];
+    }
+  }
 });
 
 
